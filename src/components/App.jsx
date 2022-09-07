4e645c925a5d357509a24324ab35d465a67ef26d
Denis Zaporozhan <zaporozhanbiz@gmail.com>
2022-09-07 19:00:48 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,6 @@ export const App = () => {
   return (
     <div
       style={{
-        height: '100%',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
