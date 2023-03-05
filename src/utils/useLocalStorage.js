0496ff512644996f3f70a25048a2c1d4ddf4e3e0
Denis Zaporozhan <zaporozhanbiz@gmail.com>
2023-03-05 13:29:20 +0200

    update

--- src/utils/useLocalStorage.js
+++ src/utils/useLocalStorage.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
 export default function useLocalStorage(key, defaultValue) {
   const [state, setState] = useState(() => {
@@ -8,6 +8,7 @@ export default function useLocalStorage(key, defaultValue) {
   useEffect(() => {
     window.localStorage.setItem(key, JSON.stringify(state));
   }, [key, state]);
+  // ?
 
   return [state, setState];
 }
